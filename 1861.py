c1f18c4f19f206c0df5d8f0e1d81759e23347b41
Elias Yishak <42216813+eliasyishak@users.noreply.github.com>
2024-11-23 12:48:47 -0500

    Init

---
+++ 1861.py
@@ -0,0 +1,51 @@
+# https://leetcode.com/problems/rotating-the-box
+from typing import List
+
+
+class Solution:
+    def rotate_the_box(self, box: List[List[str]]) -> List[List[str]]:
+
+        ROWS = len(box)
+        COLS = len(box[0])
+
+        for i in range(ROWS):
+            right = COLS - 1
+            left = right - 1
+            while right >= 0 and left >= 0:
+
+                right_val = box[i][right]
+                left_val = box[i][left]
+
+                # Ensure that the right spot is a valid spot we
+                # can swap a value with
+                if right_val != ".":
+                    right -= 1
+                    left -= 1
+                elif left_val == ".":
+                    left -= 1
+                elif left_val == "#":
+                    box[i][right] = "#"
+                    box[i][left] = "."
+                    right -= 1
+                    left -= 1
+                elif left_val == "*":
+                    left -= 1
+                    right = left
+
+        transposed_box = [[None for _ in range(ROWS)] for _ in range(COLS)]
+        for i in range(ROWS):
+            for j in range(COLS):
+                transposed_box[j][i] = box[i][j]
+
+        rotated_box = [row[::-1] for row in transposed_box]
+
+        return rotated_box
+
+
+if __name__ == "__main__":
+
+    test_case_1 = [["#", ".", ".", ".", "#"]]
+    test_case_2 = [["#", ".", "*", "."], ["#", "#", "*", "."]]
+
+    cls = Solution()
+    cls.rotate_the_box(box=test_case_2)
