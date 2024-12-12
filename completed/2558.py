8bec46df0724d459b4fe3d88ae21f4f02e881297
Elias Yishak <42216813+eliasyishak@users.noreply.github.com>
2024-12-11 22:06:15 -0500

    Create 2558.py

---
+++ completed/2558.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/take-gifts-from-the-richest-pile
+"""
+Classic heap problem
+"""
+from typing import List
+import heapq
+
+
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        gifts = [-1 * val for val in gifts]
+
+
+        heapq.heapify(gifts)
+        for _ in range(k):
+            biggest_item = heapq.heappop(gifts)
+            reduced = int(abs(biggest_item) ** 0.5)
+            heapq.heappush(gifts, -reduced)
+        
+        return sum([abs(val) for val in gifts])
+
+
+if __name__ == "__main__":
+    test_case = {
+        "gifts": [25, 64, 9, 4, 100],
+        "k": 4,
+    }  # 29
+
+    cls = Solution()
+    ans = cls.pickGifts(gifts=test_case["gifts"], k=test_case["k"])
+    print("----")
+    print(ans)
