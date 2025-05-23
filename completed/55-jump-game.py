c0c98c3c11f3525fcac49427b86b8a38d4be5e3f
Elias Yishak <elias940@gmail.com>
2025-05-23 16:43:40 -0400

    Update 55-jump-game.py

--- completed/55-jump-game.py
+++ completed/55-jump-game.py
@@ -1,96 +1,48 @@
 # https://leetcode.com/problems/jump-game
 """
-This question can be solved a few ways. You can take a BFS type of
-approach where you treat each index as a node in a graph and attempt
-to do level order traversal.
+Because this is only looking for a pass/fail, we can simply just take a greedy
+approach to the problem and look at how far we can reach based on each index
+we pass.
 
-You can also do a greedy approach where you look at the furthest you can
-go from each index and iterate through it one by one. This is the accepted
-solution since it has the lowest time complexity.
-
-I also implemented a DFS solution with memoization which doesn't meet the time
-complexity needed for this problem.
+If we end up on an index that we previously did not find a way to reach, then
+we can early exit out.
 """
-
-from typing import Dict, List
 
 
 class Solution:
-    def canJumpGreedy(self, nums: List[int]) -> bool:
+    def canJumpSlow(self, nums: list[int]) -> bool:
+        def dfs(i: int) -> bool:
+            if i == len(nums) - 1:
+                return True
+
+            for val in range(nums[i]):
+                res = dfs(i=i + val + 1)
+                if res:
+                    return True
+
+            return False
+
+        return dfs(i=0)
+
+    def canJump(self, nums: list[int]) -> bool:
+        # Define the max reachable index as a variable that we keep pushing down
+        # as we iterate through the whole list
         max_reachable_index = 0
-        for i, max_jump in enumerate(nums):
-            # If the max reachable index value is less than our
-            # current index, meaning we can't reach it, then it
-            # will not be possible to reach the end
+        for i, val in enumerate(nums):
+            # If we are at an index that we cannot reach, then we can safely exit out
             if max_reachable_index < i:
                 return False
 
-            max_reachable_index = max(max_reachable_index, i + max_jump)
+            max_reachable_index = max(max_reachable_index, i + val)
 
         return True
-
-    def canJump(self, nums: List[int]) -> bool:
-        memo: Dict[int, bool] = {}
-
-        def dfs(index) -> bool:
-            if index in memo:
-                return memo[index]
-
-            if index == len(nums) - 1:
-                return True
-
-            if index >= len(nums):
-                return False
-
-            jump = 1
-            respones = []
-            for _ in range(nums[index]):
-                _ans = dfs(index + jump)
-                memo[index] = _ans
-
-                respones.append(_ans)
-                jump += 1
-
-            return True in respones
-
-        return dfs(0)
-
-    def canJumpBFS(self, nums: List[int]) -> bool:
-        """
-        This approach is pretty slow, I attempted to solve
-        this by using a BFS approach with an iterative queue
-        """
-
-        queue = [(nums[0], 0)]
-
-        visited = set()
-        while queue:
-            q_len = len(queue)
-
-            for _ in range(q_len):
-                jumps, current_index = queue.pop(0)
-                if current_index in visited:
-                    continue
-
-                visited.add(current_index)
-
-                if current_index == len(nums) - 1:
-                    return True
-
-                for i in range(jumps):
-                    next_index = current_index + i + 1
-                    if next_index < len(nums) and next_index not in visited:
-                        queue.append((nums[next_index], next_index))
-
-        return False
 
 
 if __name__ == "__main__":
     test_case_1 = [2, 3, 1, 1, 4]  # true
     test_case_2 = [3, 2, 1, 0, 4]  # false
-    test_case_3 = [2, 0]  # true
 
     cls = Solution()
-    ans = cls.canJumpGreedy(test_case_1)
-    print("-----")
+    ans = cls.canJump(test_case_2)
+    print("-------")
     print(ans)
